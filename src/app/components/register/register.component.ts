59882c08aaa42ee55aea7dcda95c40b4be177ae2
Vadithalion <al375550@uji.es>
2020-04-14 11:52:32 +0200

    little changes to angular

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -8,8 +8,17 @@ import { Component, OnInit } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   constructor() { }
-
   ngOnInit(): void {
+    function showPass() {
+    //  let x = <HTMLInputElement>document.getElementById('inputPassword');
+      let x = (document.getElementById(inputPassword) as HTMLInputElement);
+
+      if (x.type === 'password') {
+        x.type = 'text';
+      } else {
+        x.type = 'password';
+      }
+    }
   }
 
 }
