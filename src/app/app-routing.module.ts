07792858d9f1b5611c8fbc5f51178f7f5b40b810
David Moreno Capel <davidmoreno@iMac-de-David.local>
2020-04-15 13:03:50 +0200

    Creados 2 componentes, rellenado componente Film

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,16 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { FilmInfoComponent } from './film-info/film-info.component';
 
 
 const routes: Routes = [
   {path: '', component: HomeComponent },
   {path: 'register', component: RegisterComponent},
-  {path: 'login', component: LoginComponent}
+  {path: 'login', component: LoginComponent},
+  {path: 'user', component: UserInfoComponent},
+  {path: 'film', component: FilmInfoComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LoginComponent } from './components/login/login.component';
+import { UserInfoComponent } from './user-info/user-info.component';
+import { FilmInfoComponent } from './film-info/film-info.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { LoginComponent } from './components/login/login.component';
     HomeComponent,
     RegisterComponent,
     HeaderComponent,
-    LoginComponent
+    LoginComponent,
+    UserInfoComponent,
+    FilmInfoComponent
   ],
   imports: [
     BrowserModule,
