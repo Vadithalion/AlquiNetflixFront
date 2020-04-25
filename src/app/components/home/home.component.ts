1de06d0d8f6d682544c18242e1607690f3f9e48a
David Moreno Capel <morenocapeldavid@gmail.com>
2020-04-25 19:27:49 +0200

    Añadidos cambios del home, diseño y funcionalidad

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -3,13 +3,92 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  
+  cards: Array<any> = [
+    {
+      img:
+        'https://images-na.ssl-images-amazon.com/images/I/51cfeIIm12L._AC_.jpg',
+      name: 'Star Wars',
+      txt: 'Era se una vez una galaxia muy muy lejana y estaba Don Quijote',
+    },
+    {
+      img:
+        'https://images-na.ssl-images-amazon.com/images/I/51cfeIIm12L._AC_.jpg',
+      name: 'Start Wars',
+      txt: 'Era se una vez una galaxia muy muy lejana y estaba Don Quijote',
+    },
+    {
+      img:
+        'https://images-na.ssl-images-amazon.com/images/I/51cfeIIm12L._AC_.jpg',
+      name: 'Start Wars',
+      txt: 'Era se una vez una galaxia muy muy lejana y estaba Don Quijote',
+    },
+    {
+      img:
+        'https://images-na.ssl-images-amazon.com/images/I/51cfeIIm12L._AC_.jpg',
+      name: 'Start Wars',
+      txt: 'Era se una vez una galaxia muy muy lejana y estaba Don Quijote',
+    },
+    {
+      img:
+        'https://images-na.ssl-images-amazon.com/images/I/51cfeIIm12L._AC_.jpg',
+      name: 'Start Wars',
+      txt: 'Era se una vez una galaxia muy muy lejana y estaba Don Quijote',
+    },
+    {
+      img:
+        'https://images-na.ssl-images-amazon.com/images/I/51cfeIIm12L._AC_.jpg',
+      name: 'Start Wars',
+      txt: 'Era se una vez una galaxia muy muy lejana y estaba Don Quijote',
+    },
+    {
+      img:
+        'https://images-na.ssl-images-amazon.com/images/I/51cfeIIm12L._AC_.jpg',
+      name: 'Start Wars',
+      txt: 'Era se una vez una galaxia muy muy lejana y estaba Don Quijote',
+    },
+    {
+      img:
+        'https://images-na.ssl-images-amazon.com/images/I/51cfeIIm12L._AC_.jpg',
+      name: 'Start Wars',
+      txt: 'Era se una vez una galaxia muy muy lejana y estaba Don Quijote',
+    },
+    {
+      img:
+        'https://images-na.ssl-images-amazon.com/images/I/51cfeIIm12L._AC_.jpg',
+      name: 'Start Wars',
+      txt: 'Era se una vez una galaxia muy muy lejana y estaba Don Quijote',
+    },
+    {
+      img:
+        'https://images-na.ssl-images-amazon.com/images/I/51cfeIIm12L._AC_.jpg',
+      name: 'Start Wars',
+      txt: 'Era se una vez una galaxia muy muy lejana y estaba Don Quijote',
+    },
+    {
+      img:
+        'https://images-na.ssl-images-amazon.com/images/I/51cfeIIm12L._AC_.jpg',
+      name: 'Start Wars',
+      txt: 'Era se una vez una galaxia muy muy lejana y estaba Don Quijote',
+    },
+    {
+      img:
+        'https://images-na.ssl-images-amazon.com/images/I/51cfeIIm12L._AC_.jpg',
+      name: 'Start Wars',
+      txt: 'Era se una vez una galaxia muy muy lejana y estaba Don Quijote',
+    },
+    {
+      img:
+        'https://images-na.ssl-images-amazon.com/images/I/51cfeIIm12L._AC_.jpg',
+      name: 'Start Wars',
+      txt: 'Era se una vez una galaxia muy muy lejana y estaba Don Quijote',
+    }
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
